353fcc1ee4573711038bb2bdaf28442b54c08dfd
Paper Cheung <hkpaperhk@gmail.com>
2026-05-14 22:10:48 +0800

    Remove Firestore backend support

--- utils/data_manager.py
+++ utils/data_manager.py
@@ -1,12 +1,1 @@
-import streamlit as st
-
-USE_FIRESTORE = False
-try:
-    USE_FIRESTORE = st.secrets.get("use_firestore", False)
-except Exception:
-    pass
-
-if USE_FIRESTORE:
-    from utils.data_manager_firestore import *
-else:
-    from utils.data_manager_gsheets import *
+from utils.data_manager_gsheets import *
